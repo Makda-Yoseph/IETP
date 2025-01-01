3b44a07b40ed11a29c3eaa91882e7965e3d022ab
Makda-Yoseph <makdayosephzewge@gamil.com>
2025-01-01 23:59:56 +0300

    m

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors"); // Import cors
 const app = express();
 const PORT = process.env.PORT || 3000;
-const garbageRoutes = require('./routes/rotuer.js');
+const garbageRoutes = require("./routes/router.js");
+
+// Use CORS to allow the specific URL
+const corsOptions = {
+  origin: "https://ietp-yzcn.onrender.com", // Allow this domain to make requests
+  methods: ["GET", "POST", "PUT", "DELETE"], // Allow specific methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Allow specific headers
+};
+
+app.use(cors(corsOptions)); // Use CORS with the defined options
 app.use(express.json());
 app.use("/api", garbageRoutes);
 
