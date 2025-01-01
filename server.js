83172ac6082f5e243e3290de3dc9ddb629b0f0e2
Makda-Yoseph <makdayosephzewge@gamil.com>
2025-01-02 00:42:45 +0300

    m

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors"); 
+const cors = require("cors");
 const app = express();
 const garbageRoutes = require("./routes/garbageRoute.js");
 
 const corsOptions = {
   origin: "https://ietp-yzcn.onrender.com",
-  methods: ["GET", "POST", "PUT", "DELETE"], 
-  allowedHeaders: ["Content-Type", "Authorization"], 
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 };
 
-app.use(cors(corsOptions)); 
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/api", garbageRoutes);
 app.get("/", (req, res) => {
@@ -19,9 +19,10 @@ app.get("/", (req, res) => {
     message: "Hello from the server!",
   });
 });
-const MONGODB_URL = process.env.MONGODB_URL;
 mongoose
-  .connect(MONGODB_URL) 
+  .connect(
+    "mongodb+srv://hayle_hb:HHBW1234@nodejs.b3kc1wk.mongodb.net/?retryWrites=true&w=majority&appName=NodeJS"
+  )
   .then(() => {
     console.log("Connected to MongoDB");
     app.listen(5000, () => {
